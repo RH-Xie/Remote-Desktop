ba279dac0b729013e4e6ea5b90ed499f3ec48ae8
RH-Xie <74254340+RH-Xie@users.noreply.github.com>
2022-04-30 20:52:40 +0800

    Update ddnsscript.sh

--- ddnsscript.sh
+++ ddnsscript.sh
@@ -1,8 +1,9 @@
 # <your_domain_name> : domain name get from pubyun, example: xxxx.f3322.net
 # <your_update_code> : update code from domain name settings
-# replace them in line 10 and remove angle brackets <>
+# replace them in line 13 and remove angle brackets <>
 # <your_wan_interface> : probably be 'wan' or 'eth0.2', remove angle brackets <>
- 
+# replace it in line 1 and remove angle brackets <>
+
 intf="<your_wan_interface>"
 ifconfig $intf | grep "inet addr" | awk  '{print $2}' | awk -F: '{print $2}' > /tmp/ip
 myip=$(head -1 /tmp/ip)
